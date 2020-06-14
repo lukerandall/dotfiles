c172d941fb90e7119685238b3f409e77b4ddb8f5
Luke Randall <luke.randall@gmail.com>
2020-06-14 14:48:04 +0100

    Add really skanky CSV output

--- print_settings.rb
+++ print_settings.rb
@@ -1,13 +1,17 @@
 #!/usr/bin/env ruby
 
+CSV = ARGV.include?('--csv')
+
 require 'json'
 
 json = JSON.parse(File.read('settings.json').gsub(/\/\/.*$/, ''))
 keys = json.keys.select { |k| k =~ /vim\..*Mode/ }
 keys.each do |key|
-  puts ['#', key.gsub('ModeKeyBindingsNonRecursive', '').gsub('vim.', ''), 'mode'].join(' ').upcase
-  puts
-  puts "```"
+  unless CSV
+    puts ['#', key.gsub('ModeKeyBindingsNonRecursive', '').gsub('vim.', ''), 'mode'].join(' ').upcase
+    puts
+    puts "```"
+  end
   json[key].each do |cmd|
     shortcut = cmd['before'].join(" ").ljust(22)
     commands = cmd['commands']
@@ -25,8 +29,14 @@ keys.each do |key|
     other_desc = nil
 
     output = [shortcut, desc, other_desc ]
-    puts output.join(" ").strip
+    if CSV
+      puts "\"#{output.shift.strip}\",\"#{output.join(" ").strip}\""
+    else
+      puts output.join(" ").strip
+    end
   end
-  puts "```"
-  puts
+  unless CSV
+    puts "```"
+    puts
+  end
 end

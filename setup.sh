fb9b7da6c661d0beef1bdb94aba2ae93c38c1be5
Luke Randall <luke.randall@gmail.com>
2021-11-16 14:32:38 +0000

    Add fonts brewfile

--- setup.sh
+++ setup.sh
@@ -19,3 +19,6 @@ curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
 echo "Applying macOS settings"
 ./settings.sh
+
+echo "Installing fonts"
+brew bundle --file=./Fonts.brewfile

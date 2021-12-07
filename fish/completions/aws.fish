1789cf81fa719715a4dfd5d43bd6394bda2bc9aa
Luke Randall <luke.randall@gmail.com>
2021-12-07 21:53:22 +0000

    A few fish plugins

--- fish/completions/aws.fish
+++ fish/completions/aws.fish
@@ -1,5 +1,16 @@
-function __fish_complete_aws
-    env COMP_LINE=(commandline -pc) aws_completer | tr -d ' '
+function __aws_complete_root
+  test (count (commandline -o)) = 1
 end
 
-complete -c aws -f -a "(__fish_complete_aws)"
+function __aws_complete
+  if set -q aws_completer_path
+    set -lx COMP_SHELL fish
+    set -lx COMP_LINE (commandline)
+
+    eval $aws_completer_path | command sed 's/ $//'
+  end
+end
+
+complete --command aws --no-files --arguments '(__aws_complete)'
+complete --command aws --no-files --condition '__aws_complete_root' --arguments profile -d 'Get or set current profile'
+complete --command aws --no-files --condition '__fish_seen_subcommand_from profile' --arguments '(aws profiles)'
--- fish/config.fish
+++ fish/config.fish
@@ -1,13 +1,19 @@
 fish_add_path /Applications/Postgres.app/Contents/Versions/latest/bin
 fish_add_path "$HOME/.local/bin"
 fish_add_path "$HOME/.rbenv/shims"
-fish_add_path "$HOME/.pyenv"
 fish_add_path "$HOME/.cargo/bin"
-fish_add_path /opt/homebrew/bin
 
+set fish_greeting 
 fish_vi_key_bindings
 
 zoxide init fish | source
 starship init fish | source
 
+abbr --add be bundle exec
+abbr --add serve python3 -m http.server -b localhost
+abbr --add pushf "git push origin (git branch --show-current) -f"
+abbr --add fetch_db "heroku pg:backups capture --expire && curl -o latest.dump (heroku pg:backups public-url)"
+abbr --add restore_db "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $USER latest.dump -d"
+
+alias vim nvim
 

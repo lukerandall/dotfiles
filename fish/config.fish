12a010e266c7dea24bc38c10ab08b935c12ec27d
Luke Randall <luke.randall@gmail.com>
2022-07-15 11:18:09 +0100

    Add some abbrev commands and paths

--- fish/config.fish
+++ fish/config.fish
@@ -15,6 +15,7 @@ fish_vi_key_bindings
 zoxide init fish | source
 starship init fish | source
 
+status --is-interactive; and pyenv virtualenv-init - | source
 fzf_configure_bindings --directory=\ct --git_status=\cs --git_log=\cg --history=\cr --variables=\cv
 
 abbr --add be bundle exec
@@ -24,8 +25,11 @@ abbr --add pushf "git push origin (git branch --show-current) -f"
 abbr --add fetch_db "heroku pg:backups capture --expire && curl -o latest.dump (heroku pg:backups public-url)"
 abbr --add restore_db "pg_restore --verbose --clean --no-acl --no-owner -h localhost -U $USER latest.dump -d"
 abbr --add tag-staging "git tag -d staging; git tag staging && git push --tags --force"
+abbr --add tag-test "git tag -d test; git tag test && git push --tags --force"
 abbr --add restart-screen-sharing "sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.apple.screensharing -dict Disabled -bool true; sudo launchctl unload -w /System/Library/LaunchDaemons/com.apple.screensharing.plist; sudo defaults write /var/db/launchd.db/com.apple.launchd/overrides.plist com.apple.screensharing -dict Disabled -bool false && sudo launchctl load -w /System/Library/LaunchDaemons/com.apple.screensharing.plist"
-abbr --add av aws-vault
+abbr --add main git checkout main
+abbr --add master git checkout master
+abbr --add fly flyctl
 
 alias vim nvim
 alias cat bat

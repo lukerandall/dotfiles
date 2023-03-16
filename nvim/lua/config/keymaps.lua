ae5ceb5b08f61b7293c81fb3d240680653378144
Luke Randall <luke.randall@gmail.com>
2023-03-16 14:50:00 +0000

    Add example keymap

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+-- vim.api.nvim_set_keymap("c", "%%", "path", { noremap = true })
+-- vim.api.nvim_set_keymap("c", "%%", "<cmd>", { noremap = true })
+--
+--

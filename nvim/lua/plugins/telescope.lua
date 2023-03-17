5fa49a363d668f33c950f129a66f17246f5261a0
Luke Randall <luke.randall@gmail.com>
2023-03-17 10:02:07 +0000

    Clean up telescope config

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -6,13 +6,22 @@ return {
       {
         "<leader>fp",
         function()
+          require("telescope").extensions.project.project()
+        end,
+        desc = "Find Project",
+      },
+      {
+        "<leader>fP",
+        function()
           require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
         end,
         desc = "Find Plugin File",
       },
       {
         "<leader>su",
-        "<cmd>Telescope undo<cr>",
+        function()
+          require("telescope").extensions.undo.undo()
+        end,
         desc = "Undo",
       },
       {
@@ -22,14 +31,41 @@ return {
       },
     },
     dependencies = {
+      "debugloop/telescope-undo.nvim",
+      "gbprod/yanky.nvim",
       "natecraddock/telescope-zf-native.nvim",
       "nvim-telescope/telescope-fzf-native.nvim",
-      "gbprod/yanky.nvim",
+      "nvim-telescope/telescope-project.nvim",
+      "otavioschwanck/telescope-alternate",
       config = function()
-        require("telescope").load_extension("zf-native")
+        require("telescope").load_extension("project")
+        require("telescope").load_extension("telescope-alternate")
+        require("telescope").load_extension("undo")
         require("telescope").load_extension("yank_history")
+        require("telescope").load_extension("zf-native")
       end,
     },
+    config = function()
+      require("telescope").setup({
+        extensions = {
+          project = {
+            base_dirs = { "~/Code" },
+          },
+          undo = {
+            use_delta = true,
+            side_by_side = true,
+            layout_strategy = "vertical",
+            layout_config = {
+              preview_height = 0.7,
+            },
+          },
+          ["telescope-alternate"] = {
+            mappings = {},
+            presets = { "rails", "nestjs" },
+          },
+        },
+      })
+    end,
     -- change some options
     opts = {
       defaults = {
@@ -38,20 +74,5 @@ return {
         winblend = 0,
       },
     },
-  },
-
-  {
-    "debugloop/telescope-undo.nvim",
-    config = function()
-      require("telescope").load_extension("undo")
-    end,
-  },
-
-  {
-    "zolrath/telescope-projectionist.nvim", -- forked from adalessa/telescope-projectionist.nvim
-    dependencies = { "tpope/vim-projectionist" },
-    config = function()
-      require("telescope").load_extension("projectionist")
-    end,
   },
 }

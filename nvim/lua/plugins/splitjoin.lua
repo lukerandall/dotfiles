28ecce51baf111855abeae9130649077aef63f4f
Luke Randall <luke.randall@gmail.com>
2023-03-20 12:22:30 +0000

    Add splitjoin

---
+++ nvim/lua/plugins/splitjoin.lua
@@ -0,0 +1,15 @@
+return {
+  "AndrewRadev/splitjoin.vim",
+  keys = {
+    {
+      "gS",
+      "<cmd>SplitjoinSplit<cr>",
+      desc = "Split",
+    },
+    {
+      "gJ",
+      "<cmd>SplitjoinJoin<cr>",
+      desc = "Join",
+    },
+  },
+}

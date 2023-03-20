ac93d191e7de895d59b1f7521ea9814d269f6e79
Luke Randall <luke.randall@gmail.com>
2023-03-20 16:40:12 +0000

    Add yaml companion

---
+++ nvim/lua/plugins/yaml.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "someone-stole-my-name/yaml-companion.nvim",
+    requires = {
+      { "neovim/nvim-lspconfig" },
+      { "nvim-lua/plenary.nvim" },
+      { "nvim-telescope/telescope.nvim" },
+    },
+    config = function()
+      require("telescope").load_extension("yaml_schema")
+    end,
+  },
+}

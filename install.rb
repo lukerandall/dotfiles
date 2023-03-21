dd72f41f174734921a27fd3902ff0b21fe8411e8
Luke Randall <luke.randall@gmail.com>
2023-03-21 13:43:17 +0000

    Clean up install and remove old files

--- install.rb
+++ install.rb
@@ -4,14 +4,27 @@
 #
 home = File.expand_path('~')
 
+dirs = %w[bat fish nvim]
+config_files = %w[starship.toml]
+ignored = %w[
+  install.rb README.md Brewfile Brewfile.lock.json Brewfile.local Brewfile.local.lock.json Fonts.brewfile
+  Fonts.brewfile.lock.json print_settings.rb settings.json settings.sh setup.sh keybindings.json
+]
+
 Dir['*'].each do |file|
-  next if file =~ /install|readme.md|brewfile|starship.toml|bat|fish|nvim/i
+  next if dirs.include?(file) || config_files.include?(file) || ignored.include?(file)
+
   target = File.join(home, ".#{File.basename(file)}")
   puts `ln -svf #{File.expand_path file} #{target}`
 end
 
+puts
 `mkdir -p ~/.config`
-puts `ln -svf #{File.expand_path "starship.toml"} #{home}/.config/starship.toml`
-puts `ln -svf #{File.expand_path "bat"} #{home}/.config`
-puts `ln -svf #{File.expand_path "fish"} #{home}/.config`
-puts `ln -svf #{File.expand_path "nvim"} #{home}/.config`
+config_files.each do |file|
+  puts `ln -svf #{File.expand_path file} #{home}/.config/#{file}`
+end
+
+puts
+dirs.each do |dir|
+  puts `ln -svf #{File.expand_path dir} #{home}/.config`
+end

86e6845fa7f89e4d5751b1471dc12a6d0853e0d7
Luke Randall <luke.randall@gmail.com>
2023-03-26 18:18:46 +0100

    Add treesitter context

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,32 +1,33 @@
 return {
-	{ import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		opts = {
-			ensure_installed = {
-				"bash",
-				"elixir",
-				"fish",
-				"help",
-				"html",
-				"javascript",
-				"json",
-				"lua",
-				"markdown",
-				"markdown_inline",
-				"python",
-				"query",
-				"regex",
-				"ruby",
-				"tsx",
-				"typescript",
-				"vim",
-				"yaml",
-			},
-		},
-	},
-
-	-- add jsonls and schemastore and setup treesitter for json, json5 and jsonc
-	{ import = "lazyvim.plugins.extras.lang.json" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "elixir",
+        "fish",
+        "help",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "ruby",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+  },
+  { import = "lazyvim.plugins.extras.lang.json" },
 }

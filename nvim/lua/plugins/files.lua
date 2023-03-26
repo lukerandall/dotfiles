e02b66fe3e6f9e24f24b93a345ba1a86e2cb98c3
Luke Randall <luke.randall@gmail.com>
2023-03-26 17:57:14 +0100

    Add other.vim

--- nvim/lua/plugins/files.lua
+++ nvim/lua/plugins/files.lua
@@ -12,4 +12,52 @@ return {
   {
     "jghauser/mkdir.nvim",
   },
+  {
+    "rgroli/other.nvim",
+    config = function()
+      require("other-nvim").setup({
+        mappings = {
+          "rails",
+          {
+            pattern = "(.+)/spec/(.*)/(.*)/(.*)_spec.rb",
+            target = {
+              { target = "%1/db/%3/%4.rb" },
+              { target = "%1/app/%3/%4.rb" },
+              { target = "%1/%3/%4.rb" },
+            },
+          },
+          {
+            pattern = "(.+)/spec/(.*)/(.*)_spec.rb",
+            target = {
+              { target = "%1/db/%2/%3.rb" },
+              { target = "%1/app/%2/%3.rb" },
+              { target = "%1/lib/%2/%3.rb" },
+            },
+          },
+          {
+            pattern = "(.+)/spec/(.*)/(.*)_(.*)_spec.rb",
+            target = {
+              { target = "%1/app/%4s/%3_%4.rb" },
+            },
+          },
+        },
+      })
+    end,
+    keys = {
+      {
+        "<leader>fa",
+        function()
+          vim.cmd("Other")
+        end,
+        desc = "Alternate",
+      },
+      {
+        "<leader>fA",
+        function()
+          vim.cmd("OtherVSplit")
+        end,
+        desc = "Alternate (VSplit)",
+      },
+    },
+  },
 }

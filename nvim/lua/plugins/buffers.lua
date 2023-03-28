5bda602d92adf6a54caa1ee9944faa46ed4a48e5
Luke Randall <luke.randall@gmail.com>
2023-03-28 14:14:44 +0100

    Add lsp diagnostics icon to bufferline

--- nvim/lua/plugins/buffers.lua
+++ nvim/lua/plugins/buffers.lua
@@ -5,6 +5,10 @@ return {
       require("bufferline").setup({
         options = {
           diagnostics = "nvim_lsp",
+          diagnostics_indicator = function(_, level, _, _)
+            local icon = level:match("error") and " " or " "
+            return " " .. icon
+          end,
           color_icons = false,
           show_buffer_close_icons = false,
           separator_style = "thin",

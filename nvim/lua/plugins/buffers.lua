3b8895c69b12af53d23722a6e7f8de1cb3237ca6
Luke Randall <luke.randall@gmail.com>
2023-03-28 13:45:13 +0100

    Bufferline config

--- nvim/lua/plugins/buffers.lua
+++ nvim/lua/plugins/buffers.lua
@@ -1,6 +1,21 @@
 return {
-  "tiagovla/scope.nvim",
-  config = function()
-    require("scope").setup()
-  end,
+  {
+    "akinsho/bufferline.nvim",
+    config = function()
+      require("bufferline").setup({
+        options = {
+          diagnostics = "nvim_lsp",
+          color_icons = false,
+          show_buffer_close_icons = false,
+          separator_style = "thin",
+        },
+      })
+    end,
+  },
+  {
+    "tiagovla/scope.nvim",
+    config = function()
+      require("scope").setup()
+    end,
+  },
 }

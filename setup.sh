85ada94fb3f74a3c1595ae73d2ac974e391b4d54
Luke Randall <luke.randall@gmail.com>
2024-10-04 11:09:10 +0100

    Clean up setup

--- setup.sh
+++ setup.sh
@@ -3,22 +3,11 @@
 echo "Installing dotfiles"
 ./install.rb
 
-echo "Installing Xcode and Rosetta"
-xcode-select --install
-sudo xcodebuild -license accept
-/usr/sbin/softwareupdate --install-rosetta --agree-to-license
-
 echo "Installing homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Installing apps"
 brew bundle
 
-echo "Installing nvm"
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-
 echo "Applying macOS settings"
 ./settings.sh
-
-echo "Installing fonts"
-brew bundle --file=./Fonts.brewfile

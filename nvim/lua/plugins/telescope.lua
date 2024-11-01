697cfae8e4ea26e13fb9826e874eb9658f677736
Luke Randall <luke.randall@gmail.com>
2024-11-01 14:35:25 +0000

    Add smart-open and bind to open files

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -148,6 +148,9 @@ return {
               preview_height = 0.7,
             },
           },
+          smart_open = {
+            match_algorithm = "fzf",
+          },
           ["telescope-alternate"] = {
             mappings = {},
             presets = { "rails", "rspec" },
@@ -155,6 +158,32 @@ return {
         },
       })
     end,
+  },
+  {
+    "danielfalk/smart-open.nvim",
+    config = function()
+      require("telescope").load_extension("smart_open")
+    end,
+    dependencies = {
+      "kkharji/sqlite.lua",
+      { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    },
+    keys = {
+      {
+        "<leader><leader>",
+        function()
+          require("telescope").extensions.smart_open.smart_open()
+        end,
+        desc = "Find Files",
+      },
+      {
+        "<leader>ff",
+        function()
+          require("telescope").extensions.smart_open.smart_open()
+        end,
+        desc = "Find Files",
+      },
+    },
   },
   {
     "xiyaowong/telescope-emoji.nvim",

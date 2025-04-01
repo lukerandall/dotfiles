49b5257df5ae814ee199be94c158435dd5782f44
Luke Randall <luke.randall@gmail.com>
2025-04-01 16:06:52 +0100

    Improve setup order

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
-echo "Installing dotfiles"
-./install.rb
-
 echo "Installing homebrew"
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 echo "Installing apps"
 brew bundle
+
+echo "Installing dotfiles"
+./install.rb
 
 echo "Applying macOS settings"
 ./settings.sh

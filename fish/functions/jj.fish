daf3efd4b64f5d9349be35bb690d03a009bd8e3d
Luke Randall <luke.randall@gmail.com>
2025-08-05 15:27:17 +0100

    jj: jjpr reverts -b to shorthand for --bookmark and fixes completion
    I almost never use --body, and never even remembered to use -b when I did. Plus,
    I very often incorrectly used -b for --bookmark.
    
    Also fixes completion of bookmarks that have diverged, so they don't have * suffix

--- fish/functions/jj.fish
+++ fish/functions/jj.fish
@@ -11,7 +11,7 @@ function jjpr
             case --revision -r
                 set i (math $i + 1)
                 set rev $argv[$i]
-            case --bookmark
+            case --bookmark -b
                 set i (math $i + 1)
                 set bookmark $argv[$i]
             case --base -B
@@ -21,7 +21,7 @@ function jjpr
                     set i (math $i + 1)
                     set gh_extra_args $gh_extra_args $argv[$i]
                 end
-            case --assignee -a --body -b --body-file -F --head -H --label -l --milestone -m --project -p --reviewer -r --template -T --title -t --recover
+            case --assignee -a --body --body-file -F --head -H --label -l --milestone -m --project -p --reviewer -r --template -T --title -t --recover
                 # Flags that take arguments - pass through to gh
                 set gh_extra_args $gh_extra_args $argv[$i]
                 if test $i -lt (count $argv)
@@ -34,12 +34,12 @@ function jjpr
             case --force
                 set force true
             case --help -h
-                echo "Usage: jjpr [--revision|-r REV] [--bookmark BOOKMARK] [--force] [GH_PR_CREATE_FLAGS...]"
+                echo "Usage: jjpr [--revision|-r REV] [--bookmark|-b BOOKMARK] [--force] [GH_PR_CREATE_FLAGS...]"
                 echo "  Use --fill/-f to auto-populate title and body from commit message"
                 return 0
             case '*'
                 echo "Error: Unknown argument '$argv[$i]'" >&2
-                echo "Usage: jjpr [--revision|-r REV] [--bookmark BOOKMARK] [--force] [GH_PR_CREATE_FLAGS...]" >&2
+                echo "Usage: jjpr [--revision|-r REV] [--bookmark|-b BOOKMARK] [--force] [GH_PR_CREATE_FLAGS...]" >&2
                 return 1
         end
         set i (math $i + 1)
@@ -103,11 +103,11 @@ bind \cj jjedit
 
 # Completions for jjpr
 complete -c jjpr -s r -l revision -d 'Revision to create PR for' -f -r -k -a '(jj log -r "(open() | recent() | mutable() | last(20)) & ~empty()" --no-graph -T "change_id.shortest() ++ \"\\t(\" ++ description.first_line() ++ \")\\n\"" 2>/dev/null)'
-complete -c jjpr -l bookmark -d 'Bookmark name for the PR' -f -r -k -a '(jj log -r "bookmarks()" --no-graph -T "bookmarks.join(\"\\n\") ++ \"\\n\"" 2>/dev/null)'
+complete -c jjpr -s b -l bookmark -d 'Bookmark name for the PR' -f -r -k -a '(jj log -r "bookmarks()" --no-graph -T "bookmarks.map(|b| b.name()).join(\"\\n\") ++ \"\\n\"" 2>/dev/null)'
 complete -c jjpr -l force -d 'Force move existing bookmark' -f
 
 # gh pr create flags
-complete -c jjpr -l base -s B -d 'Base branch for the PR' -f -r -k -a '(jj log -r "bookmarks()" --no-graph -T "bookmarks.join(\"\\n\") ++ \"\\n\"" 2>/dev/null)'
+complete -c jjpr -l base -s B -d 'Base branch for the PR' -f -r -k -a '(jj log -r "bookmarks()" --no-graph -T "bookmarks.map(|b| b.name()).join(\"\\n\") ++ \"\\n\"" 2>/dev/null)'
 complete -c jjpr -l draft -d 'Create a draft pull request'
 complete -c jjpr -l title -d 'Title for the pull request' -r
 complete -c jjpr -l body -d 'Body for the pull request' -r
